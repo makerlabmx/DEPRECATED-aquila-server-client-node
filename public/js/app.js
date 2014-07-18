2c52f688f4070cf747e43de0c582bfc09f218ac6
Javier Olan <javikin10@gmail.com>
2014-07-18 15:16:41 -0500

    se agrega el diseño para los dispositivos en general

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,15 @@
   var aquila;
 
   app.controller('MainController', [ '$http' , '$scope', '$Aq' ,function($http, $scope, $Aq){
-    aquila = this;    
-    aquila.test = "prueba";
+    aquila = this;        
     aquila.devices=[];
     aquila.active;
     aquila.device;
+
+    aquila.classes = {};
+    aquila.sizeClases = 0;
+
+    aquila.images=['icon-075','icon-013','icon-023'];
 
 
     aquila.seeDevice = function(id){            
@@ -24,6 +28,19 @@
       Aq(id).action(action);
     };
 
+    aquila.load = function (clase){      
+      aquila.devices=[];
+      var devs = Aq(clase);
+      for(var i = 0; i < devs.length; i++)
+      {                      
+        if(devs[i].active)  { 
+          devs[i].img= aquila.classes[devs[i].class].image; 
+          devs[i].color = "color"+((i+1)%5);
+          aquila.devices.push(devs[i]);                  
+        }        
+      } 
+    }
+
 
     $Aq.on('deviceAdded', function(){
       load();
@@ -33,17 +50,32 @@
       load();
     });
 
-    function load(){            
+    function load(){
       aquila.devices=[];
       var devs = Aq("*");
       for(var i = 0; i < devs.length; i++)
       {                      
         if(devs[i].active)  { 
-          devs[i].img= "icon-075";
+
+          if(! aquila.classes.hasOwnProperty(devs[i].class)){
+            var clase = {
+              name: devs[i].class,
+              image: aquila.images[aquila.sizeClases]
+            };
+            aquila.classes[clase.name] = clase;            
+            aquila.sizeClases++;
+            console.log(aquila.classes);
+          }
+
+
+          devs[i].img= aquila.classes[devs[i].class].image;
+          devs[i].color = "color"+((i+1)%5);
           aquila.devices.push(devs[i]);
+          
+          
         }        
-      }      
-    }
+      } 
+    }    
     
   }]);
   
